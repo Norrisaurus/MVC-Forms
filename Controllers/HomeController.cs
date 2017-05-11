08c6be6dffcfaeda72143b38d5aebfa912dbc746
Luke Norris <luken22@hotmail.co.uk>
2017-05-11 11:18:16 +0100

    HomeController Commit 110517

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcForms.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        { 
             return View();
         }
 
